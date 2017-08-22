6b7ad2bdd4fe4bc01938644c56b56eeb9934ed54
Farmer uncle <bj0327_work@163.com>
2017-08-22 17:37:11 +0800

    create application

---
+++ app/src/main/java/com/jackson/rxjavaone/FashionApplication.java
@@ -0,0 +1,35 @@
+package com.jackson.rxjavaone;
+
+import android.app.Application;
+import android.content.Context;
+
+
+/*
+* FashionApplication  2016-11-10
+* Copyright (c) 2016 HYB Co.Ltd. All right reserved.
+*/
+/*
+* 整个项目的application控制
+* @author Borje
+* @version 1.0.0
+* since 2016-11-10
+*/
+public class FashionApplication extends Application {
+    public static String cache_root_path = "";
+    private static FashionApplication instance;
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+
+    public static FashionApplication getInstance() {
+        return instance;
+    }
+}
---
+++ app/src/main/java/com/jackson/rxjavaone/activity/BasicUseActivity.java
@@ -0,0 +1,15 @@
+package com.jackson.rxjavaone.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jackson.rxjavaone.R;
+
+public class BasicUseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_use);
+    }
+}
